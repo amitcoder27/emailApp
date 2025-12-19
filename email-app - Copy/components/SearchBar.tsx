4fb4cc8ea7e5f6b9d3856fd788c4e4e4b59d4952
amitcoder27 <amitcoder27@gmail.com>
2025-12-19 16:16:08 +0530

    Update SearchBar.tsx
    css

--- email-app - Copy/components/SearchBar.tsx
+++ email-app - Copy/components/SearchBar.tsx
@@ -13,7 +13,8 @@ export default function SearchBar({
       placeholder="Search by sender or subject..."
       value={value}
       onChange={(e) => onChange(e.target.value)}
-      style={{ width: "100%", padding: 6, marginBottom: 8 }}
+       style={{ width: "100%", padding: 6, marginBottom: 8 , border: "1px solid #ccc" }}
     />
   );
 }
+
